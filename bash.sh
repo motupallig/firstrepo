41674395f2f8a2700f302200c8835d3f413e4876
root <root@ip-172-31-54-236.ec2.internal>
2016-12-17 13:30:41 +0000

    remove two files

--- bash.sh
+++ bash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 a=6
-if [ $a -eq 5 ]
+if [ $a -eq 6 ]
 then
-echo "a is equal to 5"
+echo "a is equal to 6"
 exit0
 else
 echo "a is not equal to 5"
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-<<<<<<< HEAD
-
-echo "hellow team this is for testing "
-=======
-echo "hellow team"
-echo "you got a new file"
->>>>>>> 0cf5d968aeb5aedb0ec1c3e9d30403e95d2a3381
